50d31f9ccb7dc8081b81cbd6fe134a858468b01d
AbdallahAbdoulhakim <78156004+AbdallahAbdoulhakim@users.noreply.github.com>
2024-12-27 15:00:07 +0300

    Delete middlewares directory

--- middlewares/notFound.js
+++
@@ -1,7 +0,0 @@
-const notFound = (req, res, next) => {
-  res.status(404);
-  const err = new Error(`Error 404 : Resource not found at ${req.originalUrl}`);
-  next(err);
-};
-
-export default notFound;
