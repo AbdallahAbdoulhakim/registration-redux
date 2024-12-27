3c730dbcec2d4b0664d405ea5faa6ff4ff4a4d5e
AbdallahAbdoulhakim <78156004+AbdallahAbdoulhakim@users.noreply.github.com>
2024-12-27 15:02:30 +0300

    Create authRoutes.js

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,14 @@
+import {
+  createUser,
+  loginUser,
+} from "../controller/userControl.js";
+import express from "express";
+
+const router = express.Router();
+
+router.post("/register", createUser);
+router.post("/login", loginUser);
+
+
+
+export default router;
