ef34a163f3d6e9c55d6c5d40910c06c108e7dc09
AbdallahAbdoulhakim <78156004+AbdallahAbdoulhakim@users.noreply.github.com>
2024-12-27 15:05:39 +0300

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,40 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import dbConnect from "./config/dbConnect.js";
+import notFound from "./middlewares/notFound.js";
+import errorHandler from "./middlewares/errorHandler.js";
+import { createWriteStream } from "fs";
+import morgan from "morgan";
+
+import authRouter from "./routes/authRoutes.js";
 
 dotenv.config();
 
+const logWs = createWriteStream("./logs/access.log");
+
 const PORT = process.env.PORT || 5500;
+const SERVER_URL = process.env.IP_ADDRESS || 5500;
 
 const app = express();
 
 dbConnect();
 
-app.listen(PORT, () => {
-  console.log(`Server is up and listening on PORT ${PORT}`);
+app.use(express.json());
+
+app.use(cookieParser(process.env.COOKIE_SIGNATURE));
+
+app.use(morgan("combined", { stream: logWs }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ success: true, message: "Welcome to the API Server" });
+});
+
+app.use("/api/user/", authRouter);
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(PORT, IP_ADDRESS, () => {
+  console.log(`Server is up at ${SERVER_URL} and listening on PORT ${PORT}`);
 });
